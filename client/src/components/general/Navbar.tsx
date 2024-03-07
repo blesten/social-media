36f974165e9d73ffa96c4e6c85bf59084ba7bf4e
Stanley Claudius <stanley.claudius@binus.ac.id>
2024-03-07 12:43:24 +0700

    Finalize Navbar component

--- client/src/components/general/Navbar.tsx
+++ client/src/components/general/Navbar.tsx
@@ -5,6 +5,8 @@ import { IoIosNotifications } from 'react-icons/io'
 import { HiPlus } from 'react-icons/hi'
 import { Link } from 'react-router-dom'
 import UpsertPost from '../overlay/UpsertPost'
+import { MdNotificationsOff } from 'react-icons/md'
+import { TbError404 } from 'react-icons/tb'
 
 const Navbar = () => {
   const [keyword, setKeyword] = useState('')
@@ -39,22 +41,40 @@ const Navbar = () => {
   return (
     <>
       <div className='sticky top-0 shadow-light-gray shadow-sm bg-white w-full px-16 py-5 flex justify-between gap-24 items-center z-10'>
-        <Link to='/' className='flex-1 flex items-center gap-4'>
+        <Link to='/' className='flex-1 flex items-center gap-4 outline-none'>
           <img src={`${process.env.PUBLIC_URL}/assets/logo.png`} alt='Social Sphere' className='w-10' />
           <p className='text-lg font-semibold text-blue-500'>Social Sphere</p>
         </Link>
-        <div className='flex items-center gap-3 flex-[2] bg-light-gray rounded-full py-3 px-5'>
-          <AiOutlineSearch />
-          <div className='w-full relative'>
-            <input type='text' className='outline-none w-full bg-transparent text-sm' value={keyword} onChange={e => setKeyword(e.target.value)} />
-            <p className={`absolute top-0 text-gray-400 text-sm ${keyword.length > 0 ? 'hidden' : 'block'}`}>Search for friends</p>
+        <div className='flex-[2] relative'>
+          <div className='flex items-center gap-3 bg-light-gray rounded-full py-3 px-5'>
+            <AiOutlineSearch />
+            <div className='w-full relative'>
+              <input type='text' className='outline-none w-full bg-transparent text-sm' value={keyword} onChange={e => setKeyword(e.target.value)} />
+              <p className={`absolute top-0 text-gray-400 text-sm ${keyword.length > 0 ? 'hidden' : 'block'}`}>Search for friends</p>
+            </div>
           </div>
+          {/* <div className='absolute top-full left-0 w-full mt-3 bg-white shadow-md border border-gray-200 rounded-md p-4 grid grid-cols-2 gap-4 max-h-[300px] overflow-y-auto hide-scrollbar'>
+            <div className='flex items-center justify-center flex-col text-gray-500'>
+              <TbError404 className='text-7xl text-gray-400' />
+              <p className='font-semibold text-sm'>No results found</p>
+            </div> */}
+            {/* <div className='flex items-center justify-between bg-light-gray rounded-md p-3'>
+              <div className='flex items-center gap-3'>
+                <div className='w-14 h-14 rounded-full bg-gray-200' />
+                <div>
+                  <p className='font-semibold text-sm'>John Doe</p>
+                  <p className='text-xs text-gray-500 mt-1'>@johndoe</p>
+                </div>
+              </div>
+              <button className='text-xs text-white font-semibold bg-blue-500 px-4 rounded-md py-2'>Follow</button>
+            </div> */}
+          {/* </div> */}
         </div>
         <div className='flex-1 flex justify-end gap-6'>
           <div onClick={() => setOpenUpsertPostOverlay(true)} className='cursor-pointer w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center'>
             <HiPlus className='text-lg text-gray-800' />
           </div>
-          <Link to='/conversation' className='w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center'>
+          <Link to='/conversation' className='w-10 h-10 rounded-full bg-gray-100 flex items-center justify-center outline-none'>
             <RiMessage2Fill className='text-lg text-gray-800' />
           </Link>
           <div className='relative'>
@@ -62,17 +82,21 @@ const Navbar = () => {
               <IoIosNotifications className='text-xl text-gray-800 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2' />
             </div>
             <div className={`absolute top-full right-0 bg-white border border-gray-300 shadow-md rounded-md w-[320px] mt-3 origin-top ${openNotification ? 'scale-y-100' : 'scale-y-0'} transition`}>
-              <div className='flex items-center justify-between p-4 border-b border-gray-300 cursor-pointer hover:bg-light-gray'>
+              <div className='flex items-center justify-center p-4 flex-col'>
+                <MdNotificationsOff className='text-gray-400 text-4xl' />
+                <p className='text-sm text-gray-400 font-semibold mt-3'>Notification is empty</p>
+              </div>
+              {/* <div className='flex items-center justify-between p-4 border-b border-gray-300 cursor-pointer hover:bg-light-gray'>
                 <p className='text-sm'><strong>John Doe</strong> just follows you</p>
                 <p className='text-gray-500 font-semibold text-xs'>14mins ago</p>
               </div>
               <div className='flex items-center justify-between p-4 border-b border-gray-300 cursor-pointer hover:bg-light-gray'>
                 <p className='text-sm'><strong>John Doe</strong> just follows you</p>
                 <p className='text-gray-500 font-semibold text-xs'>14mins ago</p>
-              </div>
+              </div> */}
             </div>
           </div>
-          <Link to='/feeds' className='w-10 h-10 rounded-full bg-gray-200'></Link>
+          <Link to='/feeds' className='w-10 h-10 rounded-full bg-gray-200 outline-none'></Link>
         </div>
       </div>
 
--- client/src/components/overlay/UpsertPost.tsx
+++ client/src/components/overlay/UpsertPost.tsx
@@ -1,5 +1,7 @@
+import { useState, useRef } from 'react'
 import { AiOutlineClose } from 'react-icons/ai'
 import { MdFileUpload } from 'react-icons/md'
+import { FormChanged } from '../../utils/interface'
 
 interface IProps {
   openUpsertPostOverlay: boolean
@@ -8,6 +10,26 @@ interface IProps {
 }
 
 const UpsertPost: React.FC<IProps> = ({ openUpsertPostOverlay, setOpenUpsertPostOverlay, upsertPostOverlayRef }) => {
+  const [posts, setPosts] = useState<File[]>([])
+
+  const fileInputRef = useRef() as React.MutableRefObject<HTMLInputElement>
+
+  const handleClickUpload = () => {
+    fileInputRef.current?.click()
+  }
+
+  const handleRemoveImage = (idx: number) => {
+    const newPosts = [...posts]
+    newPosts.splice(idx, 1)
+    setPosts(newPosts)
+  }
+
+  const handleChangeImages = (e: FormChanged) => {
+    const target = e.target as HTMLInputElement
+    const files = [...Object.values(target.files!)]
+    setPosts([...posts, ...files])
+  }
+
   return (
     <div className={`z-20 fixed top-0 left-0 bottom-0 right-0 bg-[rgba(0,0,0,.6)] ${openUpsertPostOverlay ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'} transition flex items-center justify-center`}>
       <div ref={upsertPostOverlayRef} className={`w-1/3 bg-white rounded-md ${openUpsertPostOverlay ? 'translate-y-0' : '-translate-y-16'} transition`}>
@@ -16,10 +38,34 @@ const UpsertPost: React.FC<IProps> = ({ openUpsertPostOverlay, setOpenUpsertPost
           <AiOutlineClose onClick={() => setOpenUpsertPostOverlay(false)} className='cursor-pointer' />
         </div>
         <div className='px-5 py-3'>
-          <div className='w-full h-[250px] bg-gray-200 rounded-md flex items-center justify-center flex-col'>
-            <MdFileUpload className='text-gray-300 text-9xl' />
-            <p className='text-sm font-semibold text-gray-500'><span className='underline text-blue-500 cursor-pointer'>Upload</span> images/videos from device</p>
-          </div>
+          {
+            posts.length < 1
+            ? (
+              <div className='w-full h-[250px] bg-gray-200 rounded-md flex items-center justify-center flex-col'>
+                <MdFileUpload className='text-gray-300 text-9xl' />
+                <p className='text-sm font-semibold text-gray-500'><span onClick={handleClickUpload} className='underline text-blue-500 cursor-pointer'>Upload</span> images/videos from device</p>
+                <input type='file' ref={fileInputRef} onChange={handleChangeImages} className='hidden pointer-events-none' accept='image/*' multiple />
+              </div>
+            )
+            : (
+              <>
+                <div className='w-full h-[250px] bg-gray-100 rounded-md grid grid-cols-4 gap-5 p-4 overflow-y-auto hide-scrollbar'>
+                  {
+                    posts.map((item, idx) => (
+                      <div className='relative'>
+                        <img src={URL.createObjectURL(item)} className='w-24 h-24 rounded-md object-cover' />
+                        <div onClick={() => handleRemoveImage(idx)} className='text-white bg-red-500 rounded-full w-5 font-semibold h-5 flex items-center justify-center text-xs -top-2 -right-2 absolute outline outline-white outline-2 cursor-pointer'>
+                          <AiOutlineClose />
+                        </div>
+                      </div>
+                    ))
+                  }
+                </div>
+                <input type='file' ref={fileInputRef} onChange={handleChangeImages} className='hidden pointer-events-none' accept='image/*' multiple />
+                <p onClick={handleClickUpload} className='underline text-xs mt-2 w-fit text-blue-500 cursor-pointer'>Upload</p>
+              </>
+            )
+          }
           <div className='mt-5'>
             <label htmlFor='caption' className='text-sm'>Caption</label>
             <textarea name='caption' id='caption' className='w-full resize-none border border-gray-300 rounded-md h-24 outline-none text-sm p-3 mt-2' />
