ad0032a635672bdb938807c9267090ec247c11e9
Stanley Claudius <stanley.claudius@binus.ac.id>
2024-11-09 21:48:32 +0700

    Fix .gitignore rules

--- client/src/utils/fetchData.ts
+++ client/src/utils/fetchData.ts
@@ -1,40 +1,44 @@
 import axios from 'axios'
 
 export const getDataAPI = async(url: string, token?: string) => {
-  const res = await axios.get(url, {
+  const res = await axios.get(`http://localhost:5000${url}`, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const postDataAPI = async(url: string, data: object, token?: string) => {
-  const res = await axios.post(url, data, {
+  const res = await axios.post(`http://localhost:5000${url}`, data, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const patchDataAPI = async(url: string, data: object, token?: string) => {
-  const res = await axios.patch(url, data, {
+  const res = await axios.patch(`http://localhost:5000${url}`, data, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
 }
 
 export const deleteDataAPI = async(url: string, token?: string) => {
-  const res = await axios.delete(url, {
+  const res = await axios.delete(`http://localhost:5000${url}`, {
     headers: {
       Authorization: `${token}`
-    }
+    },
+    withCredentials: true
   })
 
   return res
--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,11 @@ const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(cors())
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'],
+  credentials: true
+}))
 app.use(morgan('dev'))
 app.use(cookieParser())
 
